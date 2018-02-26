8dce513f47094ba5d8874a2f344c1aa5bd93ee83
Paweł Gajda <pawegio@gmail.com>
2018-02-26 23:34:00 +0100

    Rename MainActivity -> Sample1Activity

--- app/src/main/java/com/pawegio/androidprofilersamples/Sample1Activity.kt
+++ app/src/main/java/com/pawegio/androidprofilersamples/Sample1Activity.kt
@@ -5,19 +5,19 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DividerItemDecoration
 import com.elpassion.android.commons.recycler.adapters.basicAdapterWithLayoutAndBinder
 import com.elpassion.android.commons.recycler.basic.ViewHolderBinder
-import kotlinx.android.synthetic.main.main_activity.*
+import kotlinx.android.synthetic.main.sample_1_activity.*
 import kotlinx.android.synthetic.main.simple_list_item.view.*
 import org.threeten.bp.Duration
 import org.threeten.bp.LocalDateTime
 import org.threeten.bp.format.DateTimeFormatter
 
-class MainActivity : AppCompatActivity() {
+class Sample1Activity : AppCompatActivity() {
 
     private val items = mutableListOf<Item>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        setContentView(R.layout.sample_1_activity)
 
         recyclerView.adapter = basicAdapterWithLayoutAndBinder(
             items, R.layout.simple_list_item, ::bindItem
